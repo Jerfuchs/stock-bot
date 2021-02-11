e490d004f3152db8a6920afbc474fe2eb09c35d9
Jeremy Fuchs <59092235+Jerfuchs@users.noreply.github.com>
2021-02-11 10:50:56 +0100

    added voice channel join and leave

--- config/config.py
+++ config/config.py
@@ -1,2 +1,4 @@
 #Config file for embedet Bot to extend the bot to stock-bot
-# Howw does stock market works: https://www.youtube.com/watch?v=l3t406oTmss
+# How does stock market works: https://www.youtube.com/watch?v=l3t406oTmss
+
+TOKEN = 'ODA3OTEzNTk4NjEyNDA2Mjgy.YB-6LA.lbNpCx0UVwq4jOSPOkHnWfjG9Kg'
--- main.py
+++ main.py
@@ -3,22 +3,24 @@ from programy.clients.embed.basic import EmbeddedBasicBot
 import discord
 from discord.ext import commands
 from config.config import *
+import typing as t
+from optional import Optional
 
-client = commands.Bot(command_prefix='-') #Bot listening to @client.commands() / Currently not active due to on_message
+bot = commands.Bot(command_prefix='-') #Bot listening to @client.commands() / Currently not active due to on_message
 
 stock_bot = EmbeddedBasicBot() #Bot initialised/ Extend to Stock Bot
 
-@client.event #Start Signal if bot is online
+@bot.event #Start Signal if bot is online
 async def on_ready():
     print('I am alive')
-    channel = client.get_channel(500228562817908756) #general Channel Bachelor of Stupidity
+    channel = bot.get_channel(500228562817908756) #general Channel Bachelor of Stupidity
 
     #await channel.send('Hello there im stock-bot and here is my Command List:')
     #await channel.send('```In developement\n Version 0.5```' )
     #await channel.send('Or just Chat with me')
 
 
-@client.command()   #Test command
+@bot.command()   #Test command
 async def q(ctx):
     question = ctx.message.clean_content[2:]
     answer = getLastPrice(question)
@@ -30,24 +32,46 @@ async def q(ctx):
     await ctx.send(answer)
 
 
-@client.command()  #test for tagging user
+@bot.command()  #test for tagging user
 async def mention1(ctx, user: discord.Member):
   await ctx.send(user.mention + 'Kein CSGO zocken jetzt')
 
 
-@client.listen('on_message') # trigger on message (Where Bot talks with you)
+@bot.listen('on_message') # trigger on message (Where Bot talks with you)
 async def talk(ctx):
     if ctx.author.bot: #If statement so the bot doesn't message itself
         return
+
+    if (ctx.clean_content == "join"):   #Bot join (channel you are in)
+        await join(ctx)
+        return
+
+    if (ctx.clean_content == "leave"):  #Bot leave (channel you are in)
+        await leave(ctx)
+        return
+
 
     answer = stock_bot.ask_question(ctx.clean_content)
     if (answer is None): # If bot doesn't understand language = ''/ Counter empty message send error
         answer = 'I dont know what you mean'
 
-    channel = client.get_channel(ctx.channel.id)
+    channel = bot.get_channel(ctx.channel.id)
     await channel.send(answer)
 
 
-client.run('ODA3OTEzNTk4NjEyNDA2Mjgy.YB-6LA.lbNpCx0UVwq4jOSPOkHnWfjG9Kg') # actually start the bot in discord
+
+#Bot join and disconnect from server / works
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+
+@bot.command()
+async def leave(ctx):
+    await ctx.guild.voice_client.disconnect()
+
+
+bot.run(TOKEN) # actually start the bot in discord
 
 
