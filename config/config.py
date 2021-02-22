fee501581498c7498a082eb19a8b17e413c51d77
elisbardhi <OrionMarsi789>
2021-02-22 17:53:50 +0100

    Problem with chatting solved

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@
 # files = {Pfad}
 files = {'aiml': ['./storage/categories']}
 
-TOKEN = 'ODA3OTEzNTk4NjEyNDA2Mjgy.YB-6LA.3g7jpwFJUlSYJowgrF5qhDja70c'
+TOKEN = 'ODEzNDQ2MjU1Nzk0ODQ3NzY2.YDPa3A.o2pM6xoaj42_cfTWR-DKWOUYCPM√'
 
 # stock = ODEyNDExNDYwMzU2NTM4NDI4.YDAXIg.1N0LUtXGEpuHhCPhcHsIBh7-N7I
 # stock-bot = ODA3OTEzNTk4NjEyNDA2Mjgy.YB-6LA.lbNpCx0UVwq4jOSPOkHnWfjG9Kg
