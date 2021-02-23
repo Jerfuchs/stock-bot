f691ce771faa50c6fd2ab4d12c3931e19d07e71a
Texbi <niklas.venino@gmail.com>
2021-02-23 17:20:48 +0100

    'aiml'

--- config/config.py
+++ config/config.py
@@ -4,7 +4,9 @@
 # files = {Pfad}
 files = {'aiml': ['./storage/categories']}
 
-TOKEN = 'ODEzNDQ2MjU1Nzk0ODQ3NzY2.YDPa3A.o2pM6xoaj42_cfTWR-DKWOUYCPM√'
+TOKEN = 'ODEzODA1NTU4OTkzNTE4NjUz.YDUpfQ.U4q36__bkJb2Ap_CGAEtHWly-6A'
+
+# TOKEN = 'ODEzNDQ2MjU1Nzk0ODQ3NzY2.YDPa3A.o2pM6xoaj42_cfTWR-DKWOUYCPM√'
 
 # stock = ODEyNDExNDYwMzU2NTM4NDI4.YDAXIg.1N0LUtXGEpuHhCPhcHsIBh7-N7I
 # stock-bot = ODA3OTEzNTk4NjEyNDA2Mjgy.YB-6LA.lbNpCx0UVwq4jOSPOkHnWfjG9Kg
