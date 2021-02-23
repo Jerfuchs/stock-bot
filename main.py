64cedcb303a0220e02187a43d50f379222ab084f
elisbardhi <OrionMarsi789>
2021-02-23 19:10:50 +0100

    Updated translation in German

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import discord
 from discord.ext import commands
 from config.config import *
 import typing as t
+from google_trans_new import google_translator
 
 # from optional import Optional
 
+translator = google_translator()
 bot = commands.Bot(command_prefix='-')  # Bot listening to @client.commands() / Currently not active due to on_message
 
 stock_bot = EmbeddedDataFileBot(files, defaults=True)  # Bot initialised/ Extend to Stock Bot
@@ -27,7 +29,9 @@ async def talk(ctx):
     if ctx.author.bot:  # If statement so the bot doesn't message itself
         return
 
-    answer = stock_bot.ask_question(ctx.clean_content)
+    command = translator.translate(ctx.clean_content, lang_src="de", lang_tgt="en")
+    answer = stock_bot.ask_question(command)
+    answer = translator.translate(answer, lang_src="en", lang_tgt="de")
     if answer is None:  # If bot doesn't understand language = ''/ Counter empty message send error
         answer = 'I dont know what you mean'
 
